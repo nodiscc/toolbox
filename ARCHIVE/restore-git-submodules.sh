930de870fc16c343e5dfafeba9c8edebefc42c3c
nodiscc <nodiscc@gmail.com>
2019-02-23 01:55:55 +0100

    add ARCHIVE/restore-git-submodules.sh

---
+++ ARCHIVE/restore-git-submodules.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#Desription: add git submodules from a .gitmodules file
+set -e
+
+git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
+    while read path_key path
+    do
+        url_key=$(echo $path_key | sed 's/\.path/.url/')
+        url=$(git config -f .gitmodules --get "$url_key")
+        git submodule add --depth 1 -- $url $path
+    done
