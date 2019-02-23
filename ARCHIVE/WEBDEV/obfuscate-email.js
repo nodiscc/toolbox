73aac6be84154324e94700e06133b994b1a81afd
nodiscc <nodiscc@gmail.com>
2019-02-23 01:55:02 +0100

    add ARCHIVE/WEBDEV/obfuscate-email.js

---
+++ ARCHIVE/WEBDEV/obfuscate-email.js
@@ -0,0 +1,6 @@
+    <script language="javascript" type="text/javascript">
+    var pre = "hideme";
+    document.write("<a href='mailto:" + pre + "@domain.com'>" + pre
+    + "@domain.com</a>");
+    </script>
+    <noscript>Enable javascript to see our email!</noscript>
