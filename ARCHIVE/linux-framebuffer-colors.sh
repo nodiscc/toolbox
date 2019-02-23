1ec5fdae535a04a51d41c79ab283591dad76d501
nodiscc <nodiscc@gmail.com>
2019-02-23 01:55:55 +0100

    add ARCHIVE/linux-framebuffer-colors.sh

---
+++ ARCHIVE/linux-framebuffer-colors.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Description: Set linux terminal colors to something more pretty.
+# http://smithfarm-thebrain.blogspot.fr/2012/01/linux-console-text-mode-framebuffer.html
+# http://phraktured.net/linux-console-colors.html
+#TODO: use solarized color scheme
+#TODO: source it from ~/.bash_aliases
+
+if [ "$TERM" = "linux" ]; then
+    echo -en "\e]P0222222" #black
+    echo -en "\e]P8222222" #darkgrey
+    echo -en "\e]P1803232" #darkred
+    echo -en "\e]P9982b2b" #red
+    echo -en "\e]P25b762f" #darkgreen
+    echo -en "\e]PA89b83f" #green
+    echo -en "\e]P3aa9943" #brown
+    echo -en "\e]PBefef60" #yellow
+    echo -en "\e]P4324c80" #darkblue
+    echo -en "\e]PC2b4f98" #blue
+    echo -en "\e]P5706c9a" #darkmagenta
+    echo -en "\e]PD826ab1" #magenta
+    echo -en "\e]P692b19e" #darkcyan
+    echo -en "\e]PEa1cdcd" #cyan
+    echo -en "\e]P7ffffff" #lightgrey
+    echo -en "\e]PFdedede" #white
+    setterm -background black -store
+    setterm -foreground white -store
+    clear #for background artifacting
+
+fi
