2cef5d8a55a5753bfa56cccf61889f60e16e2e61
nodiscc <nodiscc@gmail.com>
2019-02-23 01:55:54 +0100

    add ARCHIVE/git-reimport-gitmodules.sh

---
+++ ARCHIVE/git-reimport-gitmodules.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Description: reimport submodules from a .gitmodules files
+# https://stackoverflow.com/a/11258810
+set -e
+
+git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
+    while read path_key path
+    do
+        url_key=$(echo $path_key | sed 's/\.path/.url/')
+        url=$(git config -f .gitmodules --get "$url_key")
+        git submodule add $url $path
+    done
