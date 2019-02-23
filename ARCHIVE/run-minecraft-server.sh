e44830a14957506e53f6b9a2c7eb4bedf8101dec
nodiscc <nodiscc@gmail.com>
2019-02-23 01:55:55 +0100

    add ARCHIVE/run-minecraft-server.sh

---
+++ ARCHIVE/run-minecraft-server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Description: run minecraft server
+#wget https://s3.amazonaws.com/Minecraft.Download/versions/1.10.2/minecraft_server.1.10.2.jar
+java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui 
