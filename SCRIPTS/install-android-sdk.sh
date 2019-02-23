de8abfd634737b1b168da27b5a701539aa147eba
nodiscc <nodiscc@gmail.com>
2019-02-23 01:57:39 +0100

    add SCRIPTS/install-android-sdk.sh

---
+++ SCRIPTS/install-android-sdk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Description: install Android SDK
+#thx http://techapple.net/2014/07/runinstall-official-android-sdk-emulator-linux-ubuntulinuxmint/
+
+cd "$HOME"
+sudo aptitude install openjdk-6-jre openjdk-6-jdk icedtea6-plugin
+wget http://dl.google.com/android/android-sdk_r24.4-linux.tgz
+tar -xvzf android-sdk_r24.4-linux.tgz
+cd ~/android-sdk-linux/tools
+./android
+
+#to run
+#export PATH=${PATH}:~/android-sdk-linux/platform-tools #add to bashrc
+#android avd
