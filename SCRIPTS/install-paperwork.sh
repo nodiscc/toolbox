e806584e6f121315c7898d338b624357636b2103
nodiscc <nodiscc@gmail.com>
2019-05-01 20:25:00 +0200

    fix paperwork install script TODO move it to dbu-utils

--- SCRIPTS/install-paperwork.sh
+++ SCRIPTS/install-paperwork.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # Description: install https://github.com/jflesch/paperwork - paper document scanning/OCR/search/management
-sudo aptitude install python3-pip python3-setuptools python3-dev python3-pil libenchant-dev python3-whoosh tesseract-ocr tesseract-ocr-fra && pip3 install paperwork && paperwork-shell chkdeps paperwork_backend && paperwork-shell chkdeps paperwork && ~/.local/bin/paperwork-shell install
+sudo aptitude install python3-pip python3-setuptools python3-dev python3-pil libenchant-dev python3-whoosh tesseract-ocr tesseract-ocr-fra && \
+pip3 install paperwork --user && \
+~/.local/bin/paperwork-shell chkdeps paperwork_backend && \
+~/.local/bin/paperwork-shell chkdeps paperwork && \
+~/.local/bin/paperwork-shell install
 echo "[INFO]: run paperwork from ~/.local/bin/paperwork"
