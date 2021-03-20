6fec37f026e96561c83390d8fb703175256fbeea
nodiscc <nodiscc@gmail.com>
2021-03-20 19:21:12 +0100

    merge thunderbird-todo.sh + thunderbird-events.sh -> thundercal, refactor/cleanup, add sorting/output options (getopts)

--- ARCHIVE/thunderbird-todo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Description: output a list of thunderbird tasks not marked COMPLETED
-
-# shellcheck disable=SC2016
-for i in ~/.thunderbird/*/calendar-data/cache.sqlite; do
-    sqlite3 "$i" 'SELECT `title` FROM `cal_todos` WHERE (`ical_status` NOT LIKE "%COMPLETED%" OR `ical_status` IS NULL) ORDER BY `priority` DESC LIMIT 0, 50000;'
-done
