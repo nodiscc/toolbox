19e5eb27c795d10a15a3f8f4e5e4b20169eef1ff
nodiscc <nodiscc@gmail.com>
2021-06-24 17:18:11 +0200

    ARCHIVE/backup-reddit-multis.py: update for praw 7.x (oauth login)

--- ARCHIVE/backup-reddit-multis.py
+++ ARCHIVE/backup-reddit-multis.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 '''
-#Description: backup reddit.com subscriptions/multireddits to a text file
-#Usage : backup-reddit-multis.py USERNAME PASSWORD
+# Description: backup reddit.com subscriptions/multireddits to a text file
+# Usage : backup-reddit-multis.py USERNAME PASSWORD CLIENT_ID CLIENT_SECRET
 # Outputs plain text lists of subreddits + multireddit RSS feed
+# Requirements: praw, ID and secret from https://old.reddit.com/prefs/apps/ > create application > personal script
 '''
 
 import praw
@@ -13,11 +14,12 @@ from sys import argv
 user_agent = "Multireddit parser 0.1"
 user_name = sys.argv[1]
 password = sys.argv[2]
-r = praw.Reddit(user_agent=user_agent)
-r.login (user_name, password, disable_warning=True)
+client_id = sys.argv[3]
+client_secret = sys.argv[4]
+r = praw.Reddit(user_agent=user_agent, client_id=client_id, client_secret=client_secret, username=user_name)
 
 print('####### MULTIS ########')
-multis = r.get_multireddits(user_name)
+multis = r.redditor(user_name).multireddits()
 for multi in multis:
     print(multi.name)
 
